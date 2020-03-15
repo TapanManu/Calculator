c870e1b4f67e25902a9d53f37c238abda7d1a812
Tapan Manu <tapanmanu2000@gmail.com>
2020-03-16 00:56:05 +0530

    text display done

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,37 +3,146 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvResult;
-    EditText etCalc;
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnplus,btnminus,btnmult,btndiv,btnequals,btnclear;
+    double num1,num2,result;
+    String s="";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvResult.findViewById(R.id.tvResult);
-        etCalc.findViewById(R.id.etCalc);
-        btn1.findViewById(R.id.btn1);
-        btn2.findViewById(R.id.btn2);
-        btn3.findViewById(R.id.btn3);
-        btn4.findViewById(R.id.btn4);
-        btn5.findViewById(R.id.btn5);
-        btn6.findViewById(R.id.btn6);
-        btn7.findViewById(R.id.btn7);
-        btn8.findViewById(R.id.btn8);
-        btn9.findViewById(R.id.btn9);
-        btn0.findViewById(R.id.btn0);
-        btnplus.findViewById(R.id.btnplus);
-        btnminus.findViewById(R.id.btnminus);
-        btnmult.findViewById(R.id.btnmult);
-        btndiv.findViewById(R.id.btndiv);
-        btnequals.findViewById(R.id.btnequals);
-        btnclear.findViewById(R.id.btnclear);
+        tvResult=findViewById(R.id.tvResult);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
+        btn0=findViewById(R.id.btn0);
+        btnplus=findViewById(R.id.btnplus);
+        btnminus=findViewById(R.id.btnminus);
+        btnmult=findViewById(R.id.btnmult);
+        btndiv=findViewById(R.id.btndiv);
+        btnequals=findViewById(R.id.btnequals);
+        btnclear=findViewById(R.id.btnclear);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s =s+"1";
+                tvResult.setText(s);
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"2";
+                tvResult.setText(s);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"3";
+                tvResult.setText(s);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"4";
+                tvResult.setText(s);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"5";
+                tvResult.setText(s);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"6";
+                tvResult.setText(s);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"7";
+                tvResult.setText(s);
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"8";
+                tvResult.setText(s);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"9";
+                tvResult.setText(s);
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+"0";
+                tvResult.setText(s);
+            }
+        });
+        btnplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+" + ";
+                tvResult.setText(s);
+            }
+        });
+        btnminus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+" - ";
+                tvResult.setText(s);
+            }
+        });
+        btnmult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+" x ";
+                tvResult.setText(s);
+            }
+        });
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=s+" / ";
+                tvResult.setText(s);
+            }
+        });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvResult.setText(" ");
+            }
+        });
+
     }
 }
